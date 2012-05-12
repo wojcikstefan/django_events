269469c58e7e9a9cb9d95df4909f09d1031eec62
Stefan Wójcik <wojcikstefan@gmail.com>
2012-05-12 15:10:00 +0200

    Sending reminders: choosing distinct emails

--- events/management/commands/send_reminders.py
+++ events/management/commands/send_reminders.py
@@ -2,6 +2,7 @@
 import datetime
 
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext_lazy as _
@@ -12,26 +13,24 @@ class Command(BaseCommand):
     help = "Sends reminders to users who have acquired tickets for events that happen within the next 7 days."
     
     def handle(self, *args, **options):
-        week_future = datetime.datetime.now() + datetime.timedelta(days=7)
-        carts = Cart.objects.filter(checked_out=True)
+        now = datetime.datetime.now()
+        week_future = now + datetime.timedelta(days=7)
         mail_list = []
-        # TODO optimize and get rid of redundancy
-        soon_events = Event.objects.filter(date_start__range=(datetime.datetime.now(),
+        # TODO optimize
+        soon_events = Event.objects.filter(date_start__range=(now,
                                                               week_future))
-        soon_orders = TicketOrder.objects.filter(ticket__event__in=soon_events).distinct('ticket__event')
-        print 'Soon orders:',soon_orders
-        for order in soon_orders:
-            event = order.ticket.event
-            user = order.cart.user
-            if user:
-                subject = _(u'[Django Events] Event coming soon')
-                message = """
-                    Hey, just wanted to let you know the event you bought the
-                    tickets for is coming soon.\n
-                    Event name: %s
-                    Starts at:  %s\n
-                    Take care,
-                    Django Events
-                """ % (event.name, event.date_start)
-                send_mail(subject, message, 'noreply@django-events.com', [user.email])
-                
+        for event in soon_events:
+            ticket_orders = TicketOrder.objects.filter(ticket__event=event)
+            emails = ticket_orders.values_list('cart__user__email').distinct()
+            emails = [e[0] for e in emails]
+            subject = _(u'[Django Events] Event coming soon')
+            message = """
+                Hey, just wanted to let you know the event you bought the
+                tickets for is coming soon.\n
+                Event name: %s
+                Starts at:  %s\n
+                Take care,
+                Django Events
+            """ % (event.name, event.date_start)
+            send_mail(subject, message, 'noreply@django-events.com', emails)
+    
